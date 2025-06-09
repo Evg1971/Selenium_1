4d7f5b3e9136cf0e3c5fccf4bf51d2c724e29d5f
Evg1971 <mosoloeve@gmail.com>
2025-06-09 15:04:08 +0300

    ДЗ: Создание скриншотов страницы

---
+++ Screen_page.py
@@ -0,0 +1,57 @@
+import time
+import datetime
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Настройка опций Chrome
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)  # Эта опция предотвращает
+# автоматическое закрытие браузера после завершения скрипта
+
+# Создание экземпляра драйвера
+driver = webdriver.Chrome(
+    options=options,
+    service=ChromeService(ChromeDriverManager().install())
+)
+
+# Основной URL
+base_url = 'https://saucedemo.com/'
+
+# Открытие страницы
+driver.get(base_url)
+
+# Находим поле ввода имени пользователя с помощью XPath выражения (кастомный метод)
+# XPATH ищет элемент input с атрибутом placeholder="Username"
+user_name = driver.find_element(By.XPATH, "//input[@placeholder='Username']")
+
+# Вводим текст "standard_user" в найденное поле ввода имени пользователя.
+user_name.send_keys("standard_user")
+
+# Находим поле ввода пароля с помощью XPath выражения(кастомный метод)
+# XPath ищет любой элемент input с атрибутом placeholder="Password"
+password = driver.find_element(By.XPATH, "//input[@placeholder='Password']")
+
+# Вводим текст "secret_sauce" в найденное поле ввода пароля.
+password.send_keys("secret_sauce")
+
+# Находим кнопку входа на странице с помощью XPath выражения
+# XPath выражение ищет элемент input с атрибутом id="login-button"
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+
+# Выполняем клик по найденной кнопке входа
+# Метод click() имитирует нажатие левой кнопки мыши на элементе
+button_login.click()
+
+time.sleep(3)
+# Получаем текущую дату и время в формате "ГГГГ.ММ.ДД-ЧЧ.ММ.СС"
+now_date = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
+
+# Формируем имя файла для скриншота
+# Соединяем строку "screenshot" с отформатированной датой и расширением .png
+name_screenshot = "screenshot" + now_date + ".png"
+
+# Сохраняем скриншот в указанную директорию
+driver.save_screenshot("C:\\Users\\Катя\\PycharmProjects\\Selenium_1\\Screen\\" + name_screenshot)
