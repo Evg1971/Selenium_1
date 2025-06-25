da8052e1ab88747e7a927ab3d2993577f409f6e1
Evg1971 <mosoloeve@gmail.com>
2025-06-25 22:06:43 +0300

    ДЗ: Взаимодействие с Check box

--- 17_Interaction_with_the_Check_box.py
+++ 17_Interaction_with_the_Check_box.py
@@ -26,27 +26,20 @@ driver.set_window_size(1920, 1080)
 # Находим и кликаем по стрелке для раскрытия меню
 driver.find_element(By.XPATH, '//button[@aria-label="Toggle"]').click()
 print("Menu Home open")  # Выводим сообщение о том, что меню открыто
-# Добавляем задержку в 2 секунды для ожидания загрузки элементов
-time.sleep(2)
-
-# Находим чекбокс "Downloads", кликаем по нему и проверяем, что он выбран
-check_box_downloads = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[3]/span/label/span[1]')
-check_box_downloads.click()  # Кликаем по чекбоксу
-check_box_downloads.is_selected()  # Проверяем, что чекбокс выбран
-print("Check Box Downloads is selected")  # Выводим сообщение о выборе чекбокса
-# Добавляем задержку в 2 секунды
-time.sleep(2)
-
-# Находим чекбокс "Documents", кликаем по нему и проверяем, что он выбран
-check_box_documents = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[2]/span/label/span[1]')
-check_box_documents.click()
-check_box_documents.is_selected()
-print("Check Box Documents is selected")
-# Добавляем задержку в 2 секунды
-time.sleep(2)
-
-# Находим чекбокс "Desktop", кликаем по нему и проверяем, что он выбран
-check_box_desktop = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[1]/span/label/span[1]')
-check_box_desktop.click()
-check_box_desktop.is_selected()
-print("Check Box Desktop is selected")
+# Добавляем задержку в 1 секунду для ожидания загрузки элементов
+time.sleep(1)
+# Находим и кликаем по чек-боксу 'Desktop'
+driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[1]/span/label/span[1]').click()
+print("Check Box Desktop is marked")
+time.sleep(1)
+# Находим и кликаем по чек-боксу 'Documents'
+driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[2]/span/label/span[1]').click()
+print("Check Box Documents is marked")
+time.sleep(1)
+# Находим и кликаем по чек-боксу 'Downloads'
+driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[3]/span/label/span[1]').click()
+print("Check Box Downloads is marked")
+# Находим чекбокс "Home" и проверяем, что он выбран
+check_box_home = driver.find_element(By.XPATH, '//input[@id="tree-node-home"]')
+assert check_box_home.is_selected(), "Check Box Home is not selected!"
+print("Check Box Home is selected")
