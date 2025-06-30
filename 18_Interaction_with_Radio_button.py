b9389cb8fe012e1b574223f37afb562c5ae26e47
Evg1971 <mosoloeve@gmail.com>
2025-06-30 12:48:09 +0300

    ДЗ: Взаимодействие с Radio Button

--- 18_Interaction_with_Radio_button.py
+++ 18_Interaction_with_Radio_button.py
@@ -25,8 +25,7 @@ time.sleep(1)
 # Находим и кликаем по тексту "Последний" напротив радиокнопки
 radio_button = driver.find_element(By.XPATH, '//*[@id="radio-group-react"]/div/div[1]/div/span[2]/div/div/span/span/div/div[5]/label/div')
 radio_button.click()
-# Сохраняем текст заголовка в переменную для проверки
-text_radio_button = radio_button.text
-assert text_radio_button == "Последний"
-# Выводим сообщение об успешной проверке заголовка радиокнопки
-print(f"Выбрана радиокнопка {text_radio_button} корректно")
+# Находим радиокнопку "Последний" и проверяем, что она выбрана
+value_radio_button = driver.find_element(By.XPATH, '//*[@id="radio-group-react"]/div/div[1]/div/span[2]/div/div/span/span/div/div[5]/label/input')
+assert value_radio_button.is_selected(), "Radio Button Последний is NOT selected"
+print(f"Radio Button {radio_button.text} is selected")
